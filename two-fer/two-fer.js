0d008009fcc09b9cb5d6d4deb746315aa65ae3a2
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2019-10-19 08:54:53 +0400

    Oct 19

---
+++ two-fer/two-fer.js
@@ -0,0 +1,13 @@
+//
+// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const twoFer = (name) => {
+  // throw new Error("Remove this statement and implement this function");
+    if (typeof name === 'string')
+      console.log('One for' + name + 'one for me.');
+    else if (name.length === 0) {
+      console.log('One for you, one for me.');
+    }
+};
