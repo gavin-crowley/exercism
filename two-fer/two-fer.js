d24f26d190ede01a2fdf74dbd72bffe05e6b4f32
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2019-10-20 10:27:22 +0400

    Oct 20v2

--- two-fer/two-fer.js
+++ two-fer/two-fer.js
@@ -1,7 +1,7 @@
 //
 // This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
 // convenience to get you started writing code faster.
-//sdfsdfs
+//comment
 
 export const twoFer = (name) => {
   // throw new Error("Remove this statement and implement this function");
