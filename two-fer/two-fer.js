dc0bfe1b98a53f3646862022aa66e36f94cf0087
Gavin Crowley <42537387+gavin-crowley@users.noreply.github.com>
2019-10-26 18:20:00 +0400

    Oct 26

--- two-fer/two-fer.js
+++ two-fer/two-fer.js
@@ -3,11 +3,19 @@
 // convenience to get you started writing code faster.
 //comment new again again again again again
 
-export const twoFer = (name) => {
-  // throw new Error("Remove this statement and implement this function");
-    if (typeof name === 'string')
-      console.log('One for' + name + 'one for me.');
-    else if (name.length === 0) {
-      console.log('One for you, one for me.');
-    }
+// export const twoFer = (name) => {
+//   // throw new Error("Remove this statement and implement this function");
+//     if (typeof name === 'string')
+//       console.log('One for' + name + 'one for me.');
+//     else if (name.length === 0) {
+//       console.log('One for you, one for me.');
+//     }
+// };
+
+export const twoFer = name => {
+  if (typeof name === "string") {
+    console.log("One for " + name + " one for me.");
+  } else if (typeof name === "undefined") {
+    console.log("One for you, one for me.");
+  }
 };
